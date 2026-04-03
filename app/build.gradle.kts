64ab1852402c28a7a05bc3bcb51a47ca95b00a82
doan036 <cindydoan28@gmail.com>
2026-04-03 16:34:51 -0700

    API Connect Start

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,6 +46,11 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
---
+++ app/src/main/java/com/popcorncoders/watchly/ui/moviedetail.kt
@@ -0,0 +1,2 @@
+package com.popcorncoders.watchly.ui
+
