92c1d3834583928ca0db9aed5dbf70f1e7eb1601
doan036 <cindydoan28@gmail.com>
2026-04-03 17:38:18 -0700

    API Continued

--- app/src/main/java/com/popcorncoders/watchly/data/remote/ApiService.kt
+++ app/src/main/java/com/popcorncoders/watchly/data/remote/ApiService.kt
@@ -1,2 +1,9 @@
 package com.popcorncoders.watchly.data.remote
 
+//
+interface ApiService {
+    @GET("movie/popular")
+    suspend fun getPopularMovies (
+        @Query("api_key") apiKey: String
+    ): MovieResponse
+}
