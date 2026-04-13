6376c83c7817b4eba30c8a6690ed0378f91bd18d
doan036 <cindydoan28@gmail.com>
2026-04-13 00:55:57 -0700

    Update MovieRepository.kt

--- app/src/main/java/com/popcorncoders/watchly/repository/MovieRepository.kt
+++ app/src/main/java/com/popcorncoders/watchly/repository/MovieRepository.kt
@@ -1,2 +1,32 @@
 package com.popcorncoders.watchly.repository
 
+import com.popcorncoders.watchly.data.local.dao.MovieDao
+import com.popcorncoders.watchly.data.local.entity.MovieEntity
+import com.popcorncoders.watchly.data.remote.ApiService
+import kotlinx.coroutines.flow.Flow
+
+class MovieRepository (
+    private val apiService: ApiService,
+    private val movieDao: MovieDao,
+) {
+    private val apiKey = "8722193d9837f70bd611fb987c977f33"
+
+    // Fetch movies from API and cache them in Room
+    suspend fun fetchAndCacheMovies() {
+        val response = apiService.getPopularMovies(apiKey)
+        val entities = response.results.map { movie ->
+            MovieEntity (
+                id = movie.id,
+                title = movie.title,
+                overview = movie.overview,
+                posterPath = movie.poster_path
+            )
+        }
+        movieDao.clearAllMovies()
+        movieDao.insertMovies(entities)
+    }
+    // Get movies from local Room database as a Flow
+    fun getMoviesFromDb() : Flow<List<MovieEntity>> {
+        return movieDao.getAllMovies()
+    }
+}
